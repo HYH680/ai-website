e83084a852bc0d0db703de08f21999c2d4f86fd2
HYH680 <2028391318@qq.com>
2026-07-12 05:12:39 +0800

    Add api/user/profile.js

---
+++ api/user/profile.js
@@ -0,0 +1,8 @@
+﻿const { handleCors, setCorsHeaders, requireAuth } = require('../_lib/auth');
+const { findUserById, getUserBalance } = require('../_lib/db');
+module.exports = requireAuth(async (req, res, user) => {
+  handleCors(req, res);
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  const balance = await getUserBalance(user.id);
+  res.json({ user: { ...user, balance } });
+});
