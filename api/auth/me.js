e137fc77ab24fd372d9fa31422ad054f586d7be9
HYH680 <2028391318@qq.com>
2026-07-12 05:12:28 +0800

    Add api/auth/me.js

---
+++ api/auth/me.js
@@ -0,0 +1,23 @@
+﻿// me.js - GET /api/auth/me
+const { handleCors, setCorsHeaders, requireAuth } = require('../_lib/auth');
+
+module.exports = requireAuth(async (req, res, user) => {
+  if (req.method !== 'GET') {
+    res.writeHead(405, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ error: 'Method not allowed' }));
+    return;
+  }
+
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(JSON.stringify({
+    user: {
+      id: user.id,
+      email: user.email,
+      name: user.name,
+      avatar: user.avatar || '',
+      balance: user.balance,
+      created_at: user.created_at,
+      last_checkin: user.last_checkin || null
+    }
+  }));
+});
